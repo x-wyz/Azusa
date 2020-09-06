6283ee8f8ad6a521dacc0b244675a65d0286164c
Wilson Ye <nextyearwillbebetter1@gmail.com>
2020-09-06 00:46:20 -0700

    navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+import Navbar from './components/navbar/navbar';
+
+import { Route, Switch } from 'react-router-dom';
+
+class App extends React.Component {
+	render() {
+		return (
+			<div className="container-main">
+				<Navbar />
+			</div>
+		)
+	}
 }
 
 export default App;
---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import './navbar.css';
+
+import { Link } from 'react-router-dom';
+
+class Navbar extends Component {
+	render(){
+		return (
+			<div className="navigation-container">
+				<div className="icon">
+					<img className="company-logo" src="#" alt=" "/>
+				</div>
+				<div className="navigation">
+					<div className="upper-section">
+						<div className="nav-link-container">
+							<Link to="/" className="nav-link">
+								<p className="nav-img">H</p>
+								<p className="nav-text">Home</p>
+							</Link>
+						</div>
+
+						<div className="nav-link-container">
+							<Link to="/stores" className="nav-link">
+								<p className="nav-img">S</p>
+								<p className="nav-text">Search</p>
+							</Link>
+						</div>
+
+						<div className="nav-link-container">
+							<Link to="/help" className="nav-link">
+								<p className="nav-img">H</p>
+								<p className="nav-text">Help</p>
+							</Link>
+						</div>
+					</div>
+
+					<div className="lower-section">
+						<div className="nav-link-container">
+							<Link to="/account" className="nav-link">
+								<p className="nav-img">P</p>
+								<p className="nav-text">Profile</p>
+							</Link>
+						</div>
+
+						<div className="nav-link-container">
+							<Link to="/about" className="nav-link">
+								<p className="nav-img">A</p>
+								<p className="nav-text">About</p>
+							</Link>
+						</div>
+					</div>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default Navbar;
